5d7c2025e92fefab1cf04e4c9171878c5db626a2
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2020-12-31 22:40:41 +0900

    [update]先手後手、勝敗判定を追加

--- src/Square.js
+++ src/Square.js
@@ -2,26 +2,13 @@ import React from "react";
 import "./styles.css";
 
 //Squareコンポーネントは制御されたコンポーネント
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => {
-          this.props.onClick();
-        }}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+//関数コンポーネント：renderメソッドだけを有する
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 //全てのマス目の状態をBoardコンポーネント内で保持するようにした
@@ -29,14 +16,19 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true //先手後手○X
     };
   }
 
   handleClick(i) {
+    //.slice()を呼んで配列のコピーを作成
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "0";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext //クリックするたびに先手後手が反転
+    });
   }
 
   renderSquare(i) {
@@ -49,7 +41,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    //現在のゲームの状態（進行中 or 勝敗決定)
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "0"}`;
+    }
 
     return (
       <div>
@@ -72,6 +71,27 @@ class Board extends React.Component {
       </div>
     );
   }
+}
+
+//いずれかのプレーヤが勝利したかどうか判定
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default class Game extends React.Component {
