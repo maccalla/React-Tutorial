eb7bb1a6b5d4fabe72a9d78522a0129eeb1162ec
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2021-01-01 02:00:54 +0900

    [update]チュートリアル完了

--- src/Square.js
+++ src/Square.js
@@ -13,46 +13,18 @@ function Square(props) {
 
 //全てのマス目の状態をBoardコンポーネント内で保持するようにした
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true //先手後手○X
-    };
-  }
-
-  handleClick(i) {
-    //.slice()を呼んで配列のコピーを作成
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? "X" : "0";
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext //クリックするたびに先手後手が反転
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    //現在のゲームの状態（進行中 or 勝敗決定)
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = `Next player: ${this.state.xIsNext ? "X" : "0"}`;
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -95,15 +67,81 @@ function calculateWinner(squares) {
 }
 
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    //.slice()を呼んで配列のコピーを作成
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "0";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext //クリックするたびに先手後手が反転
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
+
   render() {
+    //現在のゲームの状態（進行中 or 勝敗決定)
+    //ゲームの履歴管理
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    //過去の手番にジャンプする
+    //TODO:movesをコンポーネントに切り出してゲームリセットがどうかのstatusを追加してやる必要がある？
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "0"}`;
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
